05fafa5ef5788cd2e74334164f01aa8464b8d8b8
holgus103 <kubasuchan@hotmail.com>
2017-11-14 23:48:08 +0100

    Splitting into triangles

---
+++ lab3/SplitIntoTriangles.m
@@ -0,0 +1,38 @@
+function triangles = SplitIntoTriangles(A, B, C, n)
+    triangles(:, :, 1) = zeros(n*n, 3);
+    triangles(:, :, 2) = zeros(n*n, 3);
+    ca = C - A;
+    ba = B - A;
+    cb = C - B;
+    dca = ca / n;
+    dba = ba / n;
+    dcb = cb / n;
+    current = A;
+    currentLine = A;
+    ending = B;
+    i = 1;
+    while i <= (n*n)
+        triangles(i, 1, 1:2) = current;        
+        triangles(i, 2, 1:2) = current + dba;
+        triangles(i, 3, 1:2) = current + dca;
+        current = current + dba;
+        i = i +1;
+        if(i > n * n)
+            break;
+        end
+        if(current == ending)
+            ending = ending + dcb;
+            current = currentLine + dca;
+            currentLine = current;
+        else
+            triangles(i, 1, 1:2) = current;        
+            triangles(i, 2, 1:2) = current - dba + dca; 
+            triangles(i, 3, 1:2) = current + dca;
+            i = i + 1;
+        end
+        
+    end
+    
+    
+end
+
