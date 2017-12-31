68731a29cc660beada4df98578ccb708641ca156
holgus103 <holgus103@gmail.com>
2017-12-31 03:12:57 +0100

    Working Heun method with samples

--- lab6/Heun.m
+++ lab6/Heun.m
@@ -1,15 +1,20 @@
 function res = Heun(a, b, n, f, y0)
-    y = zeros(n+1, 1);
+    
+    y = zeros(n+1, size(y0, 2));
     x = zeros(n+1, 1);
     h = (b - a)/ n;
-    y(1) = y0;
+    y(1, :) = y0;
     x(1) = a;
     for i=1:n
         x(i + 1) = x(i) + h;
-        fxy = f(x(i), y(i));
-        y(i + 1) = y(i) + h/2*(fxy + f(x(i + 1), y(i) +  h * fxy));  
+        args = y(i, :);
+        for k = 1:size(y0,2)
+            args(k) = args(k) + h * f{k}(x(i), y(i, :));
+        end
+        for j=1:size(y0,2) 
+            y(i + 1, j) = y(i, j) + h/2*(f{j}(x(i), y(i, :)) + f{j}(x(i + 1), args));  
+        end
     end
-    res(1:n + 1, 1) = x;
-    res(1:n + 1, 2) = y;
+    res = [x y];
 end
 
